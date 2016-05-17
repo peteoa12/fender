41f1b2a2c7fccc727a0e3da7536d6b89a2953ddc
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-05-17 13:52:35 -0400

    first time commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+window.onload = function() {
+
+	document.body.className = "animate";
+
+}
+
+
+$("#product-display").waypoint(function() {
+    $("#product-display").addClass('animate-two');
+});
+
